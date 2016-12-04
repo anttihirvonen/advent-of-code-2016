b6d780474875a6c1f4a746df7e9cb8a03d4a6374
Antti Hirvonen <antti.hirvonen@gmail.com>
2016-12-04 16:21:46 +0200

    Make minor simplifications for day 1 solutions

--- 01_a.jl
+++ 01_a.jl
@@ -1,8 +1,7 @@
 p, d = 0, 1im
 
 for cmd in split(readline(), ", ")
-    # rotate complex
-    d = d*(cmd[1] == 'L' ? -1im : 1im)
+    d *= cmd[1] == 'L' ? -1im : 1im
     p += d*parse(Int, cmd[2:end])
 end
 
--- 01_b.jl
+++ 01_b.jl
@@ -2,7 +2,7 @@ p, d = 0, 1im
 locations = []
 
 for cmd in split(readline(), ", ")
-    d = d*(cmd[1] == 'L' ? -1im : 1im)
+    d *= cmd[1] == 'L' ? -1im : 1im
 
     for i = 1:parse(Int, cmd[2:end])
         p += d
